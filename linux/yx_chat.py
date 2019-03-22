b7369d2159bad2310332af01313e87dcd76bb4ab
yxrobert <yx_robert@163.com>
2019-03-22 13:09:56 +0800

    Update yx_chat.py

--- linux/yx_chat.py
+++ linux/yx_chat.py
@@ -99,8 +99,8 @@ def start_today_info(name, city_code):
 	itchat.send(today_msg, toUserName=name_uuid)
 
 dear_list = {
-	# u"单文博" : [13, 2, 101010300, u"swb123aa"],
-	u"Lifecoach🐳" : [13, 5, 101010300, u"yanxie1103"],
+	u"单文博" : [13, 2, 101010300, u"swb123aa"],
+	u"Lifecoach🐳": [13, 12 101010300, u"yanxie1103"],
 	# u"王洋" : [6, 15, 101010300, u"wxid_4070450704312"],
 }
 
