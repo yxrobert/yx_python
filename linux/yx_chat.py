01ca079f5d753fed20f2fdbaae81f4664457af36
yxrobert <yx_robert@163.com>
2019-04-08 21:55:30 +0800

    Update yx_chat.py

--- linux/yx_chat.py
+++ linux/yx_chat.py
@@ -107,7 +107,7 @@ def get_cons(msg, key, idx):
 	itchat.send('%s: %s' % (msg['Type'], content), msg['FromUserName'])
 
 def get_gua(msg, key, idx):
-	time.sleep(3)
+	time.sleep(9)
 	content = u"无事不起卦!"
 	itchat.send('%s: %s' % (msg['Type'], content), msg['FromUserName'])
 	time.sleep(5)
