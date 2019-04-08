0fc97adb708ec37b78d6bf494635994a5bbd2bd1
yxrobert <yx_robert@163.com>
2019-04-08 21:09:17 +0800

    Update yx_weather.py

--- linux/yx_weather.py
+++ linux/yx_weather.py
@@ -59,11 +59,11 @@ def get_gua(x=-1, y=-1):
 	# s += title + '\n'
 	# s += con + '\n'
 
-	for i in soup_texts.find_all('span', class='f16 pink'):
+	for i in soup_texts.find_all('span',class='f16 pink'):
 		print(i)
 		print(i.string)
 
-	for i in soup_texts.find_all('p', class='f14 l150'):
+	for i in soup_texts.find_all('p',class='f14 l150'):
 		print(i)
 		print(i.string)
 
