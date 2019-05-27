28d6104803b4e46efc7d1f7b39d01431799db71d
yxrobert <44914214+yxrobert@users.noreply.github.com>
2019-05-27 18:19:26 +0800

    Update yx_voice.py

--- linux/yx_voice.py
+++ linux/yx_voice.py
@@ -2,6 +2,12 @@
 # -*- coding: UTF-8 -*-
 
 from aip import AipSpeech
+from pydub import AudioSegment
+
+
+# yasm-1.3.0.tar.gz
+# tar jxvf ffmpeg-4.1.3.tar.bz2
+# ./configure --enable-shared --prefix=/monchickey/ffmpeg
 
 # 
 APP_ID = '16328862'
@@ -13,7 +19,18 @@ class yXVoice():
 	def __init__(self):
 		self.client = AipSpeech(APP_ID, API_KEY, SECRET_KEY)
 
+	def convert_fmt(self, file_path, frame_rate=16000):
+		save_path = file_path.repalce(".mp3", ".pcm")
+		mp3_version = AudioSegment.from_mp3(from_path)
+		mono = mp3_version.set_frame_rate(frame_rate).set_channels(1)
+		mono.export(save_path, format='wav', codec='pcm_s16le')
+		retrun save_path
+
+
 	def asr(self, file_path):
+		# 
+		file_path = convert_fmt(file_path)
+		
 		# 识别本地文件
 		msg = self.client.asr(get_file_content(file_path), 'pcm', 16000, {
 		    'dev_pid': 1536,
