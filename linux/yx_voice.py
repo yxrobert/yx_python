5482ccde7e15767a37ddaf9386c655c9e3e4bd78
yxrobert <44914214+yxrobert@users.noreply.github.com>
2019-05-28 15:17:07 +0800

    111

--- linux/yx_voice.py
+++ linux/yx_voice.py
@@ -39,8 +39,8 @@ class yXVoice():
 		print(msg)
 		print(msg.get("err_no"))
 		if msg.get("err_no") == 0:
-			print(msg.get("result"))
-			return msg.get("result")
+			print(msg.get("result")[2 : -2])
+			return msg.get("result")[2 : -2]
 		else:
 			return ""
 
