225100e57ceb9ce19d0042d35b1908b866499627
yxrobert <44914214+yxrobert@users.noreply.github.com>
2019-10-17 15:46:42 +0800

    Update yx_server_cmd.py

--- linux/yx_server_cmd.py
+++ linux/yx_server_cmd.py
@@ -2,11 +2,12 @@
 # -*- coding: UTF-8 -*-
 
 import os
+import commands
 
 def get_onlines():
 	cmd = "ssh -p 14036 game@39.96.244.26 tail -n 29 input/log/server_1003/chat/Info.2019-10-17_* | grep \"client connected\""
-	output = os.popen(cmd)
-	return output.read()
+	ret, output = commands.getstatusoutput(cmd)
+	return output
 
 
 def main():
